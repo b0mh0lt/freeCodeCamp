cecb4af3e9c6d7698b4e5844a7c4f642276ffe8a
M. Bomholt <root@michaelbomholt.com>
2020-01-09 22:48:30 +0100

    Update Landing Page (Links, RawGit, Bootstrap, Font Awesome)

--- responsive_web_design/landing_page/script.js
+++ responsive_web_design/landing_page/script.js
@@ -1,5 +1,6 @@
 const projectName = 'product-landing-page';
 localStorage.setItem('example_project', 'Product Landing Page');
+
 var mobile = /android|iphone|ipad|ipod|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase());
 if (mobile) {
   $('.video-container').hide();
@@ -7,7 +8,6 @@ if (mobile) {
 $('.navbar-collapse a').click(function() {
   $('.navbar-collapse').collapse('hide');
 });
-//https://gist.githubusercontent.com/davidgilbertson/0f23a53e1ec1f43f425bbb0df9db3336/raw/62269e19e5e87131b0e2df8978c550f9e11ca66f/user-tabbing-class.js
 function handleFirstTab(e) {
   if (e.keyCode === 9) {
     document.body.classList.add('user-is-tabbing');
