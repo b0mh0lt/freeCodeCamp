8486ce61ee0e3ea50257657a4c56e48f12ca85cd
Michael Bomholt <14976046+b0mh0lt@users.noreply.github.com>
2020-10-03 13:10:10 +0200

    Update Responsive Web Design

--- responsive_web_design/landing_page/script.js
+++ responsive_web_design/landing_page/script.js
@@ -1,41 +1,44 @@
-const projectName = 'product-landing-page';
-localStorage.setItem('example_project', 'Product Landing Page');
+const projectName = "product-landing-page";
+localStorage.setItem("example_project", "Product Landing Page");
 
-var mobile = /android|iphone|ipad|ipod|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase());
+var mobile = /android|iphone|ipad|ipod|blackberry|mini|windows\sce|palm/i.test(
+  navigator.userAgent.toLowerCase()
+);
 if (mobile) {
-  $('.video-container').hide();
+  $(".video-container").hide();
 }
-$('.navbar-collapse a').click(function() {
-  $('.navbar-collapse').collapse('hide');
+$(".navbar-collapse a").click(function () {
+  $(".navbar-collapse").collapse("hide");
 });
 function handleFirstTab(e) {
   if (e.keyCode === 9) {
-    document.body.classList.add('user-is-tabbing');
-    window.removeEventListener('keydown', handleFirstTab);
+    document.body.classList.add("user-is-tabbing");
+    window.removeEventListener("keydown", handleFirstTab);
   }
 }
-window.addEventListener('keydown', handleFirstTab);
+window.addEventListener("keydown", handleFirstTab);
 $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
         event.preventDefault();
-        $('html, body').animate(
+        $("html, body").animate(
           {
-            scrollTop: target.offset().top - 60
+            scrollTop: target.offset().top - 60,
           },
           1000,
-          function() {
+          function () {
             var $target = $(target);
             $target.focus();
-            if ($target.is(':focus')) {
+            if ($target.is(":focus")) {
               return false;
             }
           }
