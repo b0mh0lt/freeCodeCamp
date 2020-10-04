9aeba44a548455440eb8e20ec1b3f46af287e837
Michael Bomholt <14976046+b0mh0lt@users.noreply.github.com>
2020-10-04 16:40:58 +0200

    Update Front End Libraries

---
+++ front_end_libraries/random_quote_machine/script.js
@@ -0,0 +1,46 @@
+const projectName = "random-quote-machine";
+localStorage.setItem("example_project", "Random Quote Machine");
+
+$(document).ready(function () {
+  var quotes = [];
+  var i = 0;
+  var j;
+  $.getJSON("./quotes.json", function (data) {
+    quotes = data;
+    newQuote();
+  });
+  $("#new-quote").on("click", function () {
+    newQuote();
+  });
+  function newQuote() {
+    i = Math.floor(Math.random() * quotes.length);
+    if (i === j) {
+      while (i === j) {
+        i = Math.floor(Math.random() * quotes.length);
+      }
+    }
+    j = i;
+    getQuote(i);
+  }
+  function getQuote() {
+    MotionUI.animateIn(".card-image", "fade-in");
+    $("#image").attr("src", "./" + quotes[i].image);
+    $("#image").attr("alt", quotes[i].author);
+    $("#author").html(quotes[i].author);
+    $("#text").html(quotes[i].text);
+    $("#episode").html(quotes[i].episode);
+    $("#share-quote").attr(
+      "href",
+      "https://www.facebook.com/sharer/sharer.php?u=https://codepen.io/b0mh0lt/full/beeRYb"
+    );
+    $("#tweet-quote").attr(
+      "href",
+      "https://twitter.com/intent/tweet?text=" +
+        '"' +
+        quotes[i].text +
+        '" ―' +
+        quotes[i].author +
+        "&url=https://codepen.io/b0mh0lt/full/beeRYb"
+    );
+  }
+});
